6689859b949ff81adbb1384de28697f752591347
eliezer-chaves <chaves.eliezer@outlook.com>
2025-07-08 15:13:54 -0300

    update provider

--- backend/api-laravel/app/Providers/AppServiceProvider.php
+++ backend/api-laravel/app/Providers/AppServiceProvider.php
@@ -19,16 +19,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-         $this->configureRoutes();
+        $this->configureRoutes();
 
     }
     protected function configureRoutes()
-{
-    Route::middleware('api')
-        ->prefix('api')
-        ->group(base_path('routes/api.php'));
+    {
+        Route::middleware('api')
+            ->prefix('api')
+            ->group(base_path('routes/api.php'));
 
-    Route::middleware('web')
-        ->group(base_path('routes/web.php'));
-}
+        Route::middleware('web')
+            ->group(base_path('routes/web.php'));
+    }
 }
